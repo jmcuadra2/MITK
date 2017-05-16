2fa691f9eb601d4326b42810a214189473816936
jmcuadra2 <jmcuadra@dia.uned.es>
2017-05-16 12:38:28 +0200

    Change to use resident size instead virtual size in Linux and Mac
    I have observed discrepancies in memory usage reported by MitkWorkbench
    on different operating systems. You can read memory usage at bottom
    right corner of MitkWorkbench window.
    
    When I just launch MitkWorkbench in Linux, using MITK-2016.03.0
    compiled in release mode, an usage of about 1.80 GB is reported. In
    Windows, using MITK-2016.03.0 compiled in release mode, 150 MB are
    reported. In Mac, using MITK-2016.03.0 compiled in debug mode, 4.70 GB
    are reported.
    Apps for system monitoring report a MitkWorkbench memory usage of about
    240 MB in Linux, 150 MB in Windows and 90 MB in Mac. Therefore, I think
    that memory usage is well reported on Windows but it is incorrectly
    reported on the other systems.
    
    MitkWorkbench memory usage is reported using the function
    GetProcessMemoryUsage() located at
    Modules/Core/src/DataManagement/mitkMemoryUtilities.cpp.
    In Windows it invokes GetProcessMemoryInfo( hProcess, &pmc,
    sizeof(pmc)) and pmc.WorkingSetSize is used to read current workin

--- Modules/Core/src/DataManagement/mitkMemoryUtilities.cpp
+++ Modules/Core/src/DataManagement/mitkMemoryUtilities.cpp
@@ -18,17 +18,18 @@ See LICENSE.txt or http://www.mitk.org for details.
 
 #include <stdio.h>
 #if _MSC_VER || __MINGW32__
-#include <windows.h>
-#include <psapi.h>
+  #include <windows.h>
+  #include <psapi.h>
 #elif defined(__APPLE__)
-#include <mach/mach_host.h>
-#include <mach/mach_init.h>
-#include <mach/task.h>
-#include <sys/sysctl.h>
+  #include <mach/task.h>
+  #include <mach/mach_init.h>
+  #include <mach/mach_host.h>
+  #include <sys/sysctl.h>
 #else
-#include <sys/sysinfo.h>
-#include <unistd.h>
+  #include <sys/sysinfo.h>
+  #include <unistd.h>
 #endif
+
 
 /**
  * Returns the memory usage of the current process in bytes.
@@ -43,30 +44,31 @@ size_t mitk::MemoryUtilities::GetProcessMemoryUsage()
   size_t size = 0;
   DWORD pid = GetCurrentProcessId();
   PROCESS_MEMORY_COUNTERS pmc;
-  HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pid);
-  if (hProcess == NULL)
-    return 0;
-  if (GetProcessMemoryInfo(hProcess, &pmc, sizeof(pmc)))
+  HANDLE hProcess = OpenProcess(  PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, pid );
+  if ( hProcess == NULL )
+      return 0;
+  if ( GetProcessMemoryInfo( hProcess, &pmc, sizeof(pmc)) )
   {
     size = pmc.WorkingSetSize;
   }
-  CloseHandle(hProcess);
+  CloseHandle( hProcess );
   return size;
 #elif defined(__APPLE__)
   struct task_basic_info t_info;
   mach_msg_type_number_t t_info_count = TASK_BASIC_INFO_COUNT;
   task_info(current_task(), TASK_BASIC_INFO, (task_info_t)&t_info, &t_info_count);
-  size_t size = t_info.virtual_size;
+  size_t size = t_info.resident_size;
   return size;
 #else
   int size, res, shared, text, sharedLibs, stack, dirtyPages;
-  if (!ReadStatmFromProcFS(&size, &res, &shared, &text, &sharedLibs, &stack, &dirtyPages))
-    return (size_t)size * getpagesize();
+  if ( ! ReadStatmFromProcFS( &size, &res, &shared, &text, &sharedLibs, &stack, &dirtyPages ) )
+    return (size_t) res * getpagesize();
   else
     return 0;
 #endif
   return 0;
 }
+
 
 /**
  * Returns the total size of phyiscal memory in bytes
@@ -75,9 +77,9 @@ size_t mitk::MemoryUtilities::GetTotalSizeOfPhysicalRam()
 {
 #if _MSC_VER || __MINGW32__
   MEMORYSTATUSEX statex;
-  statex.dwLength = sizeof(statex);
-  GlobalMemoryStatusEx(&statex);
-  return (size_t)statex.ullTotalPhys;
+  statex.dwLength = sizeof (statex);
+  GlobalMemoryStatusEx (&statex);
+  return (size_t) statex.ullTotalPhys;
 #elif defined(__APPLE__)
   int mib[2];
   int64_t physical_memory;
@@ -88,7 +90,7 @@ size_t mitk::MemoryUtilities::GetTotalSizeOfPhysicalRam()
   return physical_memory;
 #else
   struct sysinfo info;
-  if (!sysinfo(&info))
+  if ( ! sysinfo( &info ) )
     return info.totalram * info.mem_unit;
   else
     return 0;
@@ -97,23 +99,21 @@ size_t mitk::MemoryUtilities::GetTotalSizeOfPhysicalRam()
 
 #ifndef _MSC_VER
 #ifndef __APPLE__
-int mitk::MemoryUtilities::ReadStatmFromProcFS(
-  int *size, int *res, int *shared, int *text, int *sharedLibs, int *stack, int *dirtyPages)
+int mitk::MemoryUtilities::ReadStatmFromProcFS( int* size, int* res, int* shared, int* text, int* sharedLibs, int* stack, int* dirtyPages )
 {
   int ret = 0;
-  FILE *f;
-  f = fopen("/proc/self/statm", "r");
-  if (f)
-  {
-    size_t ignored = fscanf(f, "%d %d %d %d %d %d %d", size, res, shared, text, sharedLibs, stack, dirtyPages);
-    ++ignored;
-    fclose(f);
-  }
-  else
-  {
+  FILE* f;
+  f = fopen( "/proc/self/statm", "r" );
+  if( f ) {
+    size_t ignored = fscanf( f, "%d %d %d %d %d %d %d", size, res, shared, text, sharedLibs, stack, dirtyPages );
+                ++ignored;
+    fclose( f );
+  } else {
     ret = -1;
   }
   return ret;
 }
 #endif
 #endif
+
+
